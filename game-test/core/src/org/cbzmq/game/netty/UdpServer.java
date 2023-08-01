ee7506c18e2ae35fa97c1fd4c60bf464738de07c
chenbiao <chenbiao7099@100.me>
2023-08-01 09:09:41 +0800

    删除

--- game-test/core/src/org/cbzmq/game/netty/UdpServer.java
+++ game-test/core/src/org/cbzmq/game/netty/UdpServer.java
@@ -116,7 +116,9 @@ public final class UdpServer {
 
         ch.writeAndFlush(new DatagramPacket(
                 byteBuf,
-                SocketUtils.socketAddress("127.0.0.1", 8088))).sync();
+//                SocketUtils.socketAddress("127.0.0.1", 8088)
+                SocketUtils.socketAddress("192.168.2.145", 8088)
+        )).sync();
 
     }
 }
