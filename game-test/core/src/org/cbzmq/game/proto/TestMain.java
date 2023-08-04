1d7b4095559de2a413d15aa7d6547e879614687f
chenbiao <chenbiao7099@100.me>
2023-08-04 09:27:52 +0800

    删除

--- game-test/core/src/org/cbzmq/game/proto/TestMain.java
+++ game-test/core/src/org/cbzmq/game/proto/TestMain.java
@@ -1,9 +1,13 @@
 package org.cbzmq.game.proto;
 
+import com.badlogic.gdx.utils.Array;
 import com.google.protobuf.ByteString;
 import org.cbzmq.game.MathUtils;
+import org.cbzmq.game.enums.MsgHeader;
 import org.cbzmq.game.model.Character;
 import org.cbzmq.game.model.Player;
+
+import java.util.Date;
 
 /**
  * @ClassName TestMain
@@ -16,13 +20,12 @@ public class TestMain {
     public static void main(String[] args) {
         Character character = new Character("chenbiao");
         Player player = new Player();
-        CharacterIntProto.Character intBuild = character.toCharacterIntProto().build();
-        CharacterProto.Character floatbuild1 = character.toCharacterProto().build();
-//        byte[] bytes = Character.toCharacterBytes(player);
-
-        System.out.println(intBuild.toByteArray().length);
-        System.out.println(floatbuild1.toByteArray().length);
-//        System.out.println(bytes.length);
+        Array<Character> characters = new Array<>();
+        characters.add(player);
+        MsgByte msgByte = new MsgByte(MsgHeader.SYNC_CHARACTERS_INFO, new Date().getTime());
+        msgByte.setCharacters(characters);
+        byte[] bytes = msgByte.toByteArray();
+        System.out.println(bytes.length);
 
     }
 }
